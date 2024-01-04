196a1ee78f8e49cb92344f0452a21e7ec4701b69
Karma-234 <busari1kamal@gmail.com>
2024-01-04 09:14:40 -0800

    Add: Custom user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    phoneNumber = models.CharField(max_length=11, unique=True)
+    userBio = models.CharField(max_length=50)
+    profileImage = models.ImageField(upload_to="userProfile", null=True)
